63538065c29448e3bb8ca3efe1888eb126f76076
ryanj89 <ryanj89@gmail.com>
2017-03-02 21:29:13 -0700

    initial commit

---
+++ migrations/20170302210329_artist.js
@@ -0,0 +1,11 @@
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('artist', (artist) => {
+    artist.increments();
+    artist.text('name');
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTableIfExists('artist');
+};
---
+++ migrations/20170302210345_track.js
@@ -0,0 +1,11 @@
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('track', (track) => {
+    track.increments();
+    track.text('name');
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTableIfExists('track');
+};
